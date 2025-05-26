fdf36064f0d795eaa3dde3f84ee4d52e8b09550a
Byron Pullutasig <byronadriano@gmail.com>
2025-05-26 09:55:30 -0400

    deleted unused files

--- src/services/index.js
+++
@@ -1,6 +0,0 @@
-// src/services/index.js
-export { httpClient } from './http';
-export { outlineService } from './outline';
-export { presentationService } from './presentation';
-export { analyticsService } from './analytics';
-export { historyService } from './history'; // Add the new history service
